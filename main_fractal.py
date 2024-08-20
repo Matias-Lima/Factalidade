239b8d4bfc4b684b74b91d582cd44f98d8e1f321
Limamat1as <limamatias2002@gmail.com>
2024-08-20 11:16:18 -0300

    Update

--- main_fractal.py
+++ main_fractal.py
@@ -8,7 +8,7 @@ import altair as alt
 from MFDFA import MFDFA
 import yfinance as yf
 from datetime import datetime
-
+from functions import singularity_spectrum, scaling_exponents, hurst_exponents
 
 # Funções -------------------------------
 
@@ -47,7 +47,7 @@ def rescaled_range_analysis(ts):
     Hurst_exponent = np.polyfit(log_n,log_R_S,1)[0]
     return Hurst_exponent
 
-
+plt.style.use('dark_background')
 
 
 # ----------------------
@@ -87,7 +87,7 @@ imagem_url = "fractal.jpg"
 st.sidebar.image(imagem_url, caption='Fractalidade', use_column_width=False, output_format="JPEG", width=280)
 
 # Menu na barra lateral
-menu = ["Main","Análise MFDFA", "Análise R/S", "Informações"]
+menu = ["Main","Análise MF-DFA", "Análise R/S", "Informações"]
 escolha = st.sidebar.selectbox("Navegue", menu)
 
 if escolha == "Main":
@@ -100,7 +100,7 @@ if escolha == "Main":
     st.write(" Evidence of H differences from a half (1/2) could be interpreted as proof that returns are not independent and that long-term memory is present (Peters 1994, 1996). This shows that the volatility of securities prices to a certain extent, there is predictability. Rachev (2010), Paolella (2016), Francq (2016) etc. have confirmed that the fractal distribution have freat adaptability in the financial market, and opened up a new path for the financial market forecast.")
     
 
-elif escolha == "Análise MFDFA":
+elif escolha == "Análise MF-DFA":
     st.subheader("Análise Multifractal Detrended Fluctuation")
 
         # Selectbox para escolher um ativo
@@ -118,11 +118,12 @@ elif escolha == "Análise MFDFA":
 
 
     # Entradas para parâmetros
-    st.sidebar.subheader("Parâmetros MFDFA")
-    q = st.sidebar.number_input("Selecione o valor de q", min_value=1, max_value=10, value=2, step=1)
+    st.sidebar.subheader("Parâmetros MF-DFA")
+
     order = st.sidebar.number_input("Ordem do ajuste polinomial", min_value=1, max_value=5, value=1, step=1)
-    min_lag_exp = st.sidebar.slider("Valor mínimo do expoente de lag", min_value=0.5, max_value=2.0, value=0.5, step=0.1)
-    max_lag_exp = st.sidebar.slider("Valor máximo do expoente de lag", min_value=2.0, max_value=5.0, value=3.0, step=0.1)
+
+    min_lag_exp = st.sidebar.slider("Valor mínimo do s", min_value=0.5, max_value=2.0, value=0.5, step=0.1)
+    max_lag_exp = st.sidebar.slider("Valor máximo do s", min_value=2.0, max_value=5.0, value=3.0, step=0.1)
     num_lags = st.sidebar.slider("Número de lags", min_value=10, max_value=100, value=50, step=5)
 
     if ticker:
@@ -134,46 +135,151 @@ elif escolha == "Análise MFDFA":
             st.write(data.tail())
 
             # Selecionar o preço de fechamento para análise
-            serie_temporal = data['Close'].dropna().values
+            serie_temporal = data['Adj Close'].dropna().pct_change().dropna().values
 
-            # Executar MFDFA
+
+            st.divider()
+            # Executar MFDFA ----------------------------------------------------------------------------------
+
+            st.subheader("Multifractal Spectrum Analysis")
+
             try:
                 st.write("Executando MFDFA...")
                 # Definindo os valores de lag com base na entrada do usuário
                 lag = np.unique(np.logspace(min_lag_exp, max_lag_exp, num_lags).astype(int))
-
                 # Calcular o (MF)DFA
+
+                st.write("Selecione o valores de q")# Sidebar inputs
+                min_value = st.number_input("Valor mínimo", min_value=-100, max_value=100, value=-60, step=1)
+                max_value = st.number_input("Valor máximo", min_value=-100, max_value=100, value=60, step=1)
+                step_value = st.number_input("Passo (step)", min_value=1, max_value=100, value=3, step=1)
+                # Generate the array based on the inputs
+                q = np.arange(min_value, max_value + step_value, step_value)
+
                 lag, dfa = MFDFA(serie_temporal, lag=lag, q=q, order=order)
-
                 # Visualizar os resultados
-                st.write("Resultados da MFDFA:")
-
-                # Plot log-log
+                n_series = dfa.shape[1]  # Número de séries em dfa
                 plt.figure(figsize=(10, 5))
-                plt.loglog(lag, dfa, 'o', label=f'MFDFA q={q}')
-                plt.xlabel("Lag")
-                plt.ylabel("DFA")
-                plt.title("MFDFA Log-Log Plot")
-                plt.legend()
+                # Loop para plotar cada série de dfa
+                for i in range(0, n_series, 5):  # Incremento de 5
+                    plt.loglog(lag, dfa[:, i], 'o', label=f'q= {q[i+1]}')
 
+                plt.xlabel("Ln_s")
+                plt.ylabel("LnFq(s)")
+                plt.title("MF-DFA Log-Log Plot")
+                plt.legend()
+                plt.show() 
+                st.pyplot(plt)
+              
+
+            
+                st.write("Hurst expoente para um q específico")
+                q_value = st.number_input("Selecione o valor de q", min_value=-100, max_value=100, value=2, step=1)
+
+                lag_value, dfa_value = MFDFA(serie_temporal, lag=lag, q=q_value, order=order)
                 # Ajuste linear para estimar o índice de Hurst
-                H_hat = np.polyfit(np.log(lag)[4:20], np.log(dfa)[4:20], 1)[0]
+                H_hat = np.polyfit(np.log(lag_value)[4:20], np.log(dfa_value)[4:20], 1)[0]
 
                 # Exibir o valor estimado de H
                 st.write(f'Estimated H = '+'{:.3f}'.format(H_hat[0]))
 
+            except Exception as e:
+                st.error(f"Erro na análise MF-DFA: {e}")
+       
+# ------------------------------------------------------------------------------------------
+            st.divider()
+            try:
+                
+                st.subheader("Sources of Multifractality")
+
+                # Calcular o espectro de singularidade (Singularity Spectrum)
+                alpha, f_alpha = singularity_spectrum(lag, dfa, q)
+
+
+
+
+                st.write(""" 
+                            
+    Singularity spectrum                   
+    -------
+    alpha: np.array
+        Singularity strength `α`. The width of this function indicates the
+        strength of the multifractality. A width of `max(α) - min(α) ≈ 0`
+        means the data is monofractal.
+
+    f: np.array
+        Singularity spectrum `f(α)`. The location of the maximum of `f(α)`
+        (with `α` as the abscissa) should be 1 and indicates the most
+        prominent fractal scale in the data.
+                         """)
+
+                # Plotar o espectro de singularidade
+                plt.figure(figsize=(10, 5))
+                plt.plot(alpha, f_alpha, 'o-')
+                plt.xlabel('α (Força de Singularity)')
+                plt.ylabel('f(α) (Espectro de Singularity)')
+                plt.title('Singularity Spectrum')
+                st.pyplot(plt)
+
+                # Calcular os expoentes de escalabilidade (Scaling Exponents)
+                tau_q = scaling_exponents(lag, dfa, q)
+
+                st.write(""" 
+                             
+    Scaling exponents                  
+    -------                           
+    q: np.array
+        The `q` powers.
+
+    tau: np.array
+        Scaling exponents `τ(q)`. A usually increasing function of `q` from
+        which the fractality of the data can be determined by its shape. A
+        truly linear tau indicates monofractality, whereas a curved one
+        (usually curving around small `q` values) indicates multifractality.
+                         """)
+
+                # Plotar os expoentes de escalabilidade
+                plt.figure(figsize=(10, 5))
+                plt.plot(tau_q[0], tau_q[1], 'o-')
+                plt.xlabel('q (Ordem do Momento)')
+                plt.ylabel('τ(q) (Expoente de Escalabilidade)')
+                plt.title('Scaling Exponents')
+                st.pyplot(plt)
+
+                # Calcular os expoentes de Hurst generalizados (Hurst Exponents)
+                hurst_q = hurst_exponents(lag, dfa, q)
+
+                st.write(""" 
+                             
+    Hurst exponents               
+    -------                                         
+    q: np.array
+        The `q` powers.
+
+    hq: np.array
+        Singularity strength `h(q)`. The width of this function indicates the
+        strength of the multifractality. A width of `max(h(q)) - min(h(q)) ≈ 0`
+        means the data is monofractal.
+                         """)
+
+                # Plotar os expoentes de Hurst
+                plt.figure(figsize=(10, 5))
+                plt.plot(hurst_q[0], hurst_q[1], 'o-')
+                plt.xlabel('q (Ordem do Momento)')
+                plt.ylabel('H(q) (Expoente de Hurst)')
+                plt.title('Generalized Hurst Exponents')
                 st.pyplot(plt)
 
             except Exception as e:
-                st.error(f"Erro na análise MFDFA: {e}")
+                st.error(f"Erro na análise Sources of Multifractality: {e}")
         else:
             st.error("Erro ao carregar dados. Verifique o ticker selecionado ou a conectividade de rede.")
+
+
 
 elif escolha == "Análise R/S":
     st.subheader("Rescaled Range Analysis (R/S)")
     st.write(" A Method for Detecting Persistence, Randomness, or Mean Reversion in Financial Markets")
-
-
 
     ativo = st.selectbox("Escolha um ativo", list(tickers.keys()) + ["Outro (inserir ticker)"])
 
@@ -215,3 +321,4 @@ elif escolha == "Análise R/S":
 elif escolha == "Informações":
     st.subheader("Contato")
     st.write("Para mais informações, entre em contato conosco.")
+
